d418f4c60d63aa73e891e75ac13d9c907d6c6eec
Kiguchi118 <kiguchi118@gmail.com>
2020-10-11 16:52:24 +0900

    add: コメント画面のroute

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "@/views/Home.vue";
+import Comment from "@/views/Comment.vue";
 
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       path: "/",
       name: "Home",
       component: Home,
+    },
+    {
+      path: "/comment",
+      name: "Comment",
+      component: Comment,
     },
   ],
 });
